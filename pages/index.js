5748526d0994f2ea9805ab0a85ebda67ebfec668
Tuan Tran <quoctuanuit@gmail.com>
2023-02-27 05:01:47 +0100

    index js

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import styles from "@/styles/Home.module.css"
+import ManualHeader from "@/components/ManualHeader"
+import Header from "../components/Header"
+import LotteryEntrance from "../components/LotteryEntrance"
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Raffle</title>
+                <meta name="description" content="Web3 lotterry" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <LotteryEntrance />
+        </div>
+    )
+}
